e32880f08067cd0919cd25058c0c7785c04220d4
dwilches13 <daniel.wilches@kellogg.northwestern.edu>
2022-02-21 14:22:19 +0000

    2nd Commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-places = Place.create([{name: "Cincinnati"},{name: "Snowmass"},{name: "Denver"}])
-Post.create(title: "Visited brewery", description: "Went to a brewery in OTR and got some drinks.", posted_on: "February 12, 2022", place_id: places.first)
-Post.create(title: "Went skiing", description: "Rented skii gear and hit the hills with some friends.", posted_on: "December 14, 2021", place_id: places.second)
-Post.create(title: "Louis the Child Concert", description: "Rode a party bus with the other kwestees to Red Rocks, where we watch Louis the Child play.", posted_on: "August 28, 2021", place_id: places.third)
+#places = Place.create([{name: "Cincinnati"},{name: "Snowmass"},{name: "Denver"}])
+#Post.create(title: "Visited brewery", description: "Went to a brewery in OTR and got some drinks.", posted_on: "February 12, 2022", place_id: places.first)
+#Post.create(title: "Went skiing", description: "Rented skii gear and hit the hills with some friends.", posted_on: "December 14, 2021", place_id: places.second)
+#Post.create(title: "Louis the Child Concert", description: "Rode a party bus with the other kwestees to Red Rocks, where we watch Louis the Child play.", posted_on: "August 28, 2021", place_id: places.third)
